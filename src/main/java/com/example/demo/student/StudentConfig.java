eb03ae9d72f2377f78a2544c6a9fd1231ef2f35b
Julian Gaston <103215878+JGDevelopments@users.noreply.github.com>
2022-04-14 01:14:16 -0500

    Pre- Packaging, Testing, Post Mapping and some more functional logic
    Really just the bare bones :)

---
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,35 @@
+package com.example.demo.student;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
+        return args -> {
+            Student julian = new Student(
+                    "Julian",
+                    "jgaston001@my.wilmu.edu",
+                    LocalDate.of(2002, Month.MARCH, 13)
+            );
+
+            Student jack = new Student(
+                    "Jack",
+                    "jackie@my.wilmu.edu",
+                    LocalDate.of(1900, Month.JANUARY, 5)
+            );
+
+            repository.saveAll(
+                    List.of(julian, jack)
+            );
+        };
+    }
+}
